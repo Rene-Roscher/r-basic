ca74a949ca620ac759a38c3e11f1da0c086c52b1
Rene Roscher <roscher794@gmail.com>
2020-09-09 02:50:36 +0200

    Added fontawesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
 
     /* Tools */
     .browserSync('localhost:8000')
