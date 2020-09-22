26bcc1a5e9fc67c261deb6122b6d5fadda9c498b
Rene Roscher <roscher794@gmail.com>
2020-09-22 08:16:06 +0200

    Added Button State Support

--- resources/js/rservices/request.js
+++ resources/js/rservices/request.js
@@ -1,5 +1,6 @@
 import {notify} from './';
-const handle = async (request) => {
+const handle = async (request, button = null) => {
+    var state = rservices.buttonState.setPending(button);
     let response = await request;
     let keys = Object.keys(response.messages);
     if (keys.length > 0)
@@ -8,6 +9,7 @@ const handle = async (request) => {
         });
     if (response.redirect != null) setTimeout(() => location.href = response.redirect, 300);
     rservices.cache.isRequesting = false;
+    state.setDone();
     return response;
 };
 
@@ -15,40 +17,40 @@ const request = {
     get(url, params) {
         return {
             request: axios.get(url, {params}).then(res => res.data).catch(e => e.response.data),
-            async handle() {
-                return await handle(this.request);
+            async handle(button) {
+                return await handle(this.request, button);
             },
         };
     },
     post(url, data) {
         return {
             request: axios.post(url, data).then(res => res.data).catch(e => e.response.data),
-            async handle() {
-                return await handle(this.request);
+            async handle(button) {
+                return await handle(this.request, button);
             }
         };
     },
     put(url, data) {
         return {
             request: axios.put(url, data).then(res => res.data).catch(e => e.response.data),
-            async handle() {
-                return await handle(this.request);
+            async handle(button) {
+                return await handle(this.request, button);
             }
         };
     },
     patch(url, data) {
         return {
             request: axios.patch(url, data).then(res => res.data).catch(e => e.response.data),
-            async handle() {
-                return await handle(this.request);
+            async handle(button) {
+                return await handle(this.request, button);
             }
         };
     },
     delete(url) {
         return {
             request: axios.delete(url).then(res => res.data).catch(e => e.response.data),
-            async handle() {
-                return await handle(this.request);
+            async handle(button) {
+                return await handle(this.request, button);
             }
         };
     },
