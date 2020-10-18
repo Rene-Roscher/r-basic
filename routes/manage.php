dbaee725d25bc80f9fbe55e12f91226117b7a043
Rene Roscher <roscher794@gmail.com>
2020-10-18 22:01:19 +0200

    Update manage.php

--- routes/manage.php
+++ routes/manage.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'manage.dashboard')->name('manage.dashboard');
 Route::crud(\RServices\User::class);
+Route::crud(\RServices\Models\Permission::class);
+Route::crud(\RServices\Models\Session::class);
 Route::profile();
