a3880916da96000109ca64fa6bf18322115a105c
Rene Roscher <roscher794@gmail.com>
2020-10-22 01:22:05 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use RServices\Helpers\Button\ButtonBuilder;
+use RServices\Http\Controllers\Crud\CrudController;
 use RServices\Models\Model;
 use RServices\User;
 
@@ -33,38 +34,48 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-        Router::macro('crud', function ($model) {
-            $name = sprintf('%s.%s', 'manage', strtolower(basename($model)));
-            \Route::prefix(Str::kebab(Str::plural(basename($model))))->group(function () use ($model, $name) {
-                \Route::get('/', fn() => viewDataTables(\route(sprintf('%s.list', $name)), array_keys($model::$dataTablesFields), array_values($model::$dataTablesFields), $model::getDataTablesButtons()))->name("$name.view");
-                \Route::get('/list', fn() => $model::datatables()
-                    ->addColumn('action', fn($entry) => ButtonBuilder::create()
-                        ->addEdit(\route(sprintf('%s.edit', $name), compact('entry')))
-                        ->addDelete(\route(sprintf('%s.delete', $name), compact('entry')))
-                        ->make())
-                    ->make())->name("$name.list");
-                \Route::get('/create', fn(Request $request) => $model::createForm(\route(sprintf('%s.create', $name))))->name("$name.create");
+        Router::macro('crud', function ($model, $withPermission = true) {
+            $name = sprintf('%s.%s', 'manage', $crudName = strtolower(basename($model)));
+            \Route::prefix(Str::kebab(Str::plural(basename($model))))->group(function () use ($model, $name, $withPermission, $crudName) {
+                $middleware = sprintf('permission:%s', $crudName);
+                \Route::get('/', fn() => viewDataTables(\route(sprintf('%s.list', $name)), array_keys($model::$dataTablesFields), array_values($model::$dataTablesFields), $model::getTableViewButtons()))
+                    ->middleware($withPermission ? "$middleware.list" : [])->name("$name.view");
+
+                \Route::get('/list', fn() => $model::toDataTables($model, $name))
+                    ->middleware($withPermission ? "$middleware.list" : [])->name("$name.list");
+
+                \Route::get('/create', fn(Request $request) => $model::createForm(\route(sprintf('%s.create', $name))))
+                    ->middleware($withPermission ? "$middleware.create" : [])->name("$name.create");
+
                 \Route::post('/create', function (Request $request) use ($model, $name) {
                     if (array_key_exists('password', $request->all()))
                         if ($model == User::class) $request->offsetSet('password', \Hash::make($request->all()['password']));
                         else $request->offsetSet('password', encrypt($request->all()['password']));
                     ($entry = new $model($request->all()))->save();
                     return respond()->addMessage($entry->createdMessage(), 'success')->setRedirect(\route(sprintf('%s.edit', $name), compact('entry')))->response();
-                })->name("$name.create");
-                \Route::prefix('{entry}')->group(function () use ($model, $name) {
-                    \Route::get('/edit', fn(Request $request, $entry) => $model::findOrFail($entry)->updateForm(\route(sprintf('%s.update', $name), compact('entry'))))->name("$name.edit");
+                })->middleware($withPermission ? "$middleware.create" : [])->name("$name.create");
+
+                \Route::prefix('{entry}')->group(function () use ($model, $name, $withPermission, $middleware) {
+                    \Route::get('/edit', fn(Request $request, $entry) => $model::findOrFail($entry)->updateForm(\route(sprintf('%s.update', $name), compact('entry'))))
+                        ->middleware($withPermission ? "$middleware.show" : [])->name("$name.edit");
+
                     \Route::post('/update', function (Request $request, $entry) use ($model) {
                         $entry = $model::findOrFail($entry);
                         $entry->update($request->all());
                         return respond()->addMessage($entry->updatedMessage(), 'success')->response();
-                    })->name("$name.update");
+                    })->middleware($withPermission ? "$middleware.edit" : [])->name("$name.update");
+
                     \Route::get('/delete', function (Request $request, $entry) use ($model) {
                         $entry = $model::findOrFail($entry);
                         $entry->delete();
                         return respond()->addMessage($entry->deletedMessage(), 'success')->response();
-                    })->name("$name.delete");
+                    })->middleware($withPermission ? "$middleware.delete" : [])->name("$name.delete");
                 });
             });
+            Route::prefix('user/{entry}')->group(function () {
+                Route::get('signInto', [CrudController::class, 'signInto'])->middleware("permission:user.signinto")->name('manage.user.signInto');
+            });
+            Route::get('signBack', [CrudController::class, 'signBack'])->name('manage.user.signBack');
         });
         Router::macro('profile', function () {
             \Route::get('/profile', fn() => user()->updateForm(\route('manage.profile.update')))->name("manage.profile.view");
@@ -84,9 +95,9 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
-
         $this->mapAuthWeb('manage', '\Manage');
+
+        $this->mapWebRoutes();
     }
 
     /**
